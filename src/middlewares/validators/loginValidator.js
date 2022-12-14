487976070c5b357abe6715600b94ff77326c93f9
FP-Coding <felipe14062000@gmail.com>
2022-12-14 16:25:18 -0300

    feat: criação dos middlewares de validação do login

---
+++ src/middlewares/validators/loginValidator.js
@@ -0,0 +1,28 @@
+const passwordValidator = (req, res, next) => {
+  const { password } = req.body;
+  if (!password || password.length === 0) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });  
+  }
+  
+  if (password.length <= 5) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });  
+  }
+  next();
+};
+
+const emailValidator = (req, res, next) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+
+module.exports = {
+  passwordValidator,
+  emailValidator,
+};
