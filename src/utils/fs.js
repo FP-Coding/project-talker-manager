477d2a97b4eefb923c1633407060a2530d376f45
FP-Coding <felipe14062000@gmail.com>
2022-12-14 15:35:19 -0300

    feat: criação da função writeFileTalker

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -3,9 +3,28 @@ const { join } = require('path');
 
 const PATH = join(__dirname, '..', 'talker.json');
 
-const readFileTalker = () => {
+const readFileTalker = async () => {
   try {
-    const content = fs.readFile(PATH, 'utf-8');
+    const content = await fs.readFile(PATH, 'utf-8');
+    return JSON.parse(content);
+  } catch (error) {
+    console.log(error.message);
+    return undefined;
+  }
+};
+
+const writeFileTalker = async (content) => {
+  try {
+    const talkers = readFileTalker();
+    const newTalker = {
+      id: talkers.length + 1,
+      ...content,
+    };
+    const updatedFile = [
+      ...talkers,
+      newTalker,
+    ]; 
+    await fs.writeFile(PATH, JSON.stringify(updatedFile));
     return JSON.parse(content);
   } catch (error) {
     console.log(error.message);
@@ -15,4 +34,5 @@ const readFileTalker = () => {
 
 module.exports = {
   readFileTalker,
+  writeFileTalker,
 };
