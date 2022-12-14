1a6307ba5111925fda6780aaab89766dc28b228c
FP-Coding <felipe14062000@gmail.com>
2022-12-14 16:25:49 -0300

    feat: utilização dos middlewares de validação na rota /login

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 const generateToken = require('../utils/generateToken');
+const { emailValidator, passwordValidator } = require('../middlewares/validators/loginValidator');
 
 const loginRoute = Router();
 
-loginRoute.post('/', (_req, res) => {
+loginRoute.post('/',
+  emailValidator,
+  passwordValidator,
+  (_req, res) => {
   const token = generateToken();
   return res.status(200).json({ token });
-});
+  });
 
 module.exports = {
   loginRoute,
