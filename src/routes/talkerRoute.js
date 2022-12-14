4bc94136411075635f224a71445846b3c238080c
FP-Coding <felipe14062000@gmail.com>
2022-12-14 15:47:11 -0300

    feat: criação da rota get /talker/:id

--- src/routes/talkerRoute.js
+++ src/routes/talkerRoute.js
@@ -8,6 +8,14 @@ talkerRoute.get('/', async (_req, res) => {
   res.status(200).json(talkers);
 });
 
+talkerRoute.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await manipulateTalkers.readFileTalker();
+  const talkerById = talkers.find(({ id: idTalker }) => idTalker === Number(id));
+  if (!talkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talkerById);
+});
+
 module.exports = {
   talkerRoute,
 };
